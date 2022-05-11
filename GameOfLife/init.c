65dbb887bc32da9d7244accdeac0a31fc54ec928
robot-fmb <sc20m2f@leeds.ac.uk>
2022-05-11 19:07:27 +0800

    Configure window size, configure SDL rendering window

--- GameOfLife/init.c
+++ GameOfLife/init.c
@@ -5,14 +5,29 @@
 
 
 //The rules of the game
+static SDL_Surface* halscreen, * buffer_screen;
+static SDL_Rect winSize;
+#define _VIDEO_BPP 32
 
 //Draw squares
 
-//Draw squares
-
+SDL_Rect getWindowsSize();
+void initData();
 int main(int argc, char *argv[])
 {
 	//Initialization window
+	int    done;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	IMG_Init(IMG_INIT_PNG);
+
+	winSize = getWindowsSize();
+	//initData();
 
 	//Initialize square
 		//Draw  square
--- GameOfLife/main.c
+++ GameOfLife/main.c
@@ -4,6 +4,27 @@
 
 
 //Define window
+#define  WINDOW_WIDTH 480 
+#define  WINDOW_HEIGHT 320
 
+#define  GRID_SIZE 20  //Cell size
+#define  DEF_DENSITY 4  //Density
+
+#define X_CNT (WINDOW_WIDTH/GRID_SIZE) 
+#define Y_CNT (WINDOW_HEIGHT/GRID_SIZE)
 
 //Computational cell multiplication
+
+static int map[X_CNT][Y_CNT];
+static Uint32 lasttime = 0;
+
+SDL_Rect getWindowsSize()
+{
+	SDL_Rect s;
+	s.w = WINDOW_WIDTH;
+	s.h = WINDOW_HEIGHT;
+	s.x = 0;
+	s.y = 0;
+
+	return s;
+}
