7b25e6acf4d85a0098d138024372d273fbb9153d
robot-fmb <sc20m2f@leeds.ac.uk>
2022-05-20 16:30:11 +0800

    Structura optimization of Cell reproduction

--- GameOfLife/gfx.h
+++ GameOfLife/gfx.h
@@ -5,3 +5,9 @@
 
 
 //define method
+#pragma once
+
+
+void SDL_putpixel(int x, int y, int color);
+void SDL_drawLine(int sx, int sy, int dx, int dy, int color);
+void SDL_fillRect(int x, int y, int w, int h, int color);
